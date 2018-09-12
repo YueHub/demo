42f4010f38e277e56a205bf4867e87e8bda254c9
YueHub <li_chuan_yue@foxmail.com>
2018-09-12 11:11:07 +0800

    update learn-cpp-demo

---
+++ learn-cpp-demo/hello_world.cpp
@@ -0,0 +1,14 @@
+//
+// Created by yue on 18-8-21.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!";
+
+    return 0;
+}
+
---
+++ learn-cpp-demo/sizeof_type.cpp
@@ -0,0 +1,20 @@
+//
+// Created by yue on 18-8-21.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "size of char: " << sizeof(char) << endl;
+    cout << "size of int: " << sizeof(int) << endl;
+    cout << "size of short int: " << sizeof(short int) << endl;
+    cout << "size of long int: " << sizeof(long int) << endl;
+    cout << "size of float: " << sizeof(float) << endl;
+    cout << "size of double: " << sizeof(double) << endl;
+    cout << "size of wchat_t: " << sizeof(wchar_t) << endl;
+
+    return 0;
+}
+
