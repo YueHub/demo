e240a7717e7ccc67e821138f35968b03464fa976
YueHub <li_chuan_yue@foxmail.com>
2020-08-10 23:44:01 +0800

    ZigZag 编码测试函数

---
+++ learn-cpp-demo/ZigZagEncode32.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+
+// ZigZag 编码
+inline uint32_t ZigZagEncode32(int32_t n) {
+  // 右移为算数右移
+  // 左移时需要先将 n 转成 uint32 类型，防止溢出
+  // 当 n 为正数时 result = 2 * n
+  // 当 n 为负数时 result = - (2 * n + 1)
+  return (static_cast<uint32_t>(n) << 1) ^ static_cast<uint32_t>(n >> 31);
+}
+
+int main() {
+  uint32_t n = 1;
+
+  while (n != 0) {
+    cout << "输入 n(输入 0 结束):" << endl;
+    cin >> n;
+    cout << "result: " << ZigZagEncode32(n) << endl;
+  }
+  return 0;
+}
